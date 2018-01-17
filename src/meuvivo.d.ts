2ac5596a043b00be6c9ff487832f21f38cb8260a
R.Frega <robertofrega@gmail.com>
2018-01-17 14:51:22 -0200

    declaring extended interfaces specific to Meu Vivo

--- src/meuvivo.d.ts
+++ src/meuvivo.d.ts
@@ -3,7 +3,36 @@ import {PageData} from "./interfaces/PageData"
 import {UserData} from "./interfaces/UserData"
 import {FeatureData, FeatureInteractionData} from "./interfaces/FeatureData";
 
+declare interface MeuVivoEnvironmentData extends EnvironmentData {
+    /**
+     * A descriptive name for the environment in a dasherized form
+     * e.g.: 'meu-vivo-mais'
+     */
     name: 'meu-vivo-movel-desktop' | 'meu-vivo-movel-mobile' | 'meu-vivo-fixo-desktop-vivo-1' | 'meu-vivo-fixo-desktop-vivo-2' | 'meu-vivo-fixo-mobile-vivo-1' | 'vivo-next' | 'vivo-mais'
+}
+
+declare interface MeuVivoPageData extends PageData {
+    currentSubscription: Subscription
+}
+
+declare interface Subscription {
+    id: string
+    plan: {
+        name: string
+        billing: 'postpaid' | 'prepaid' | 'control'
+        type: 'mobile' | 'broadband' | 'landline' | 'tv'
+        technology: '4g' | 'fiber' | 'dth' | 'dsl'
+    }
+}
+
+declare interface MeuVivoUserData extends UserData {
+    subscriptions: Subscription[]
+}
+
+declare interface MessageData {
+    type: 'warning' | 'success' | 'info' | 'error'
+    name: string
+    code: string
 }
 
 export declare interface dataLayer {
@@ -15,8 +44,8 @@ export declare interface dataLayer {
     push(pageInitObject: {
         event: 'page-init'
         environment: MeuVivoEnvironmentData
-        page: PageData
-        user?: UserData
+        page: MeuVivoPageData
+        user?: MeuVivoUserData
     })
 
     /**
@@ -24,7 +53,7 @@ export declare interface dataLayer {
      *  MUST be triggered on EVERY step of the feature
      * @param featureStepObject IMPORTANT: The `event` key  must be set to `"feature-step"`
      */
-     push(featureStepObject: {
+    push(featureStepObject: {
         event: 'feature-step'
         feature: FeatureData
     })
@@ -34,10 +63,15 @@ export declare interface dataLayer {
      *  MUST be triggered on EVERY interaction with the feature
      * @param featureInteractionObject IMPORTANT: The `event` key  must be set to `"feature-interaction"`
      */
-     push(featureInteractionObject: {
+    push(featureInteractionObject: {
         event: 'feature-interaction'
         feature: FeatureData,
         interaction: FeatureInteractionData
+    })
+
+    push(interfaceMessageObject: {
+        event: 'interface-message'
+        message: MessageData
     })
 
 }
