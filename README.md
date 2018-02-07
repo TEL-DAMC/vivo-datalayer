21ddaaaeaaaf96d6c0cd33222ca0c2d1e1c82fb0
R.Frega <robertofrega@gmail.com>
2018-02-07 18:59:03 -0200

    Gerador v1.0
    Instruções no README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # vivo-datalayer
 Datalayer definitions for Vivo Telefonica Brazil
+
+# Gerador de .js a partir de .csv:
+
+Para rodar é necessário ter o Node instalado.
+
+Executar o comando `node generate-dl.js gen --file ./csv/example.csv`, onde o parâmetro `--file` é o arquivo de referência. O arquivo gerado terá o mesmo nome, na mesma pasta, mas com a extensão .js
--- generate-dl.js
+++ generate-dl.js
@@ -1,5 +1,7 @@
 'use strict'
-var argv = require('yargs')
+const fs = require('fs')
+const parser = require('csv-parse')
+const argv = require('yargs')
   .usage('Usage: $0 <command> [options]')
   .command('gen', 'Gera o arquivo js a partir do csv')
   .example('$0 gen -f meuvivo.csv', 'gera o arquivo meuvivo.js')
@@ -11,17 +13,108 @@ var argv = require('yargs')
   .alias('h', 'help')
   .argv
 
-var fs = require('fs')
-var util = require('util')
-var parser = require('csv-parse')
+const inputFilePath = argv.file
 
-var contents = fs.readFileSync(argv.file, 'utf-8')
-console.log(contents)
+const outputFilePath = argv.output || argv.file.replace(/(\.csv)$/, '.js')
 
-parser(contents, {columns: true}, function cb (err, output) {
+const csvInputFileContent = fs.readFileSync(inputFilePath, 'utf-8')
+
+const parserConfig = {
+  columns: true,
+  relax_column_count: true,
+  delimiter: ';',
+  auto_parse: true
+}
+
+parser(csvInputFileContent, parserConfig, function cb (err, output) {
   if (err) {
     console.error(err)
   } else {
-    console.log(util.inspect(output))
+    const tags = output.map(function (tag) {
+      if (tag['event']) {
+        return buildString(tag)
+      }
+    })
+    fs.writeFileSync(outputFilePath, tags.join('\n'))
   }
 })
+
+function parseValue (str) {
+  return str
+}
+
+function buildString (tag) {
+  if (tag.event.includes('feature-interaction')) {
+    return `
+              /**
+               * ${tag['FEATURE']}
+               * - ${tag['MICRO-FEATURE']}
+               */
+              dataLayer.push({
+                event: ${parseValue(tag['event'])},
+                feature: {
+                  name: ${parseValue(tag['feature.name'])},
+                  type: ${parseValue(tag['feature.type'])},
+                  step: {
+                    name: ${parseValue(tag['step.name'])},
+                    index: ${parseValue(tag['step.index'])},
+                    type: ${parseValue(tag['step.type'])},
+                    success: ${parseValue(tag['step.success'])}
+                  }
+                },
+                interaction: {
+                  name: ${parseValue(tag['interaction.name'])},
+                  value: ${parseValue(tag['interaction.value'])}
+                }
+              })
+              `
+  } else if (tag.event.includes('feature-step')) {
+    return `
+              /**
+               * ${tag['FEATURE']}
+               * - ${tag['MICRO-FEATURE']}
+               */
+              dataLayer.push({
+                event: ${parseValue(tag['event'])},
+                feature: {
+                  name: ${parseValue(tag['feature.name'])},
+                  type: ${parseValue(tag['feature.type'])},
+                  step: {
+                    name: ${parseValue(tag['step.name'])},
+                    index: ${parseValue(tag['step.index'])},
+                    type: ${parseValue(tag['step.type'])},
+                    success: ${parseValue(tag['step.success'])}
+                  }
+                }
+              })  
+              `
+  } else if (tag.event.includes('interface-message')) {
+    return `
+              /**
+               * ${tag['FEATURE']}
+               * - ${tag['MICRO-FEATURE']}
+               */
+              dataLayer.push({
+                event: 'interface-message',
+                message: {
+                  type: ${parseValue(tag['message.name'])},
+                  /**
+                   * Name of the warning.
+                   * Please try to consider including the field that showed the error.
+                   */
+                  name: string,
+                  /**
+                   * Warning code, if available
+                   */
+                  code: string
+                },
+                /**
+                 * The element that diplays the error, when available
+                 */
+                element: HTMLElement
+              })  
+              `
+  } else {
+    console.warn('Evento não reconhecido: ', tag.event)
+  }
+}
