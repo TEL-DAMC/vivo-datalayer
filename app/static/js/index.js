20f2bb36cba10b00b61ce3440b0b0c72a2f44479
R.Frega <robertofrega@gmail.com>
2018-02-19 14:43:24 -0300

    style: Adiciona o prefixo da window para declarações globais

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,5 +1,5 @@
 // TODO: Incluir um campo para seleção do arquivo a abrir
-fetch('/csv?file=example.csv').then(function (response) {
+window.fetch('/csv?file=example.csv').then(function (response) {
   return response.json()
 }).then(function (json) {
   /**
@@ -17,7 +17,7 @@ fetch('/csv?file=example.csv').then(function (response) {
    * @type {HTMLElement}
    */
   const CONTAINER = document.getElementById('table-container')
-  window.THE_TABLE = new Handsontable(CONTAINER, {
+  window.THE_TABLE = new window.Handsontable(CONTAINER, {
     data: json,
     colHeaders: headers,
     contextMenu: true,
@@ -30,10 +30,10 @@ document.getElementById('save').addEventListener('click', function (event) {
   let sendableData = window.csvData.slice()
   sendableData.unshift(window.headers)
   // TODO: Incluir um campo para nome do arquivo
-  fetch('/save-csv?file=example.csv', {
+  window.fetch('/save-csv?file=example.csv', {
     method: 'POST',
     body: JSON.stringify({data: sendableData}),
-    headers: new Headers({
+    headers: new window.Headers({
       'Content-Type': 'application/json'
     })
   }).then(res => res.json())
@@ -42,5 +42,5 @@ document.getElementById('save').addEventListener('click', function (event) {
 })
 
 document.getElementById('generate-js').addEventListener('click', function (event) {
-  fetch('/generate-tags?input=example.csv&output=example.new.js').then()
+  window.fetch('/generate-tags?input=example.csv&output=example.new.js').then()
 })
