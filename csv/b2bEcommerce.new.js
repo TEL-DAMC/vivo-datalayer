245638ea11f1eff5a326516a7e0d2c19e6e8d371
Tuca <arthurborbalopes@gmail.com>
2018-02-28 18:28:17 -0300

    first commit of b2bEcommerce.csv
    csv kinda messy. will clean it on next commig

---
+++ csv/b2bEcommerce.new.js
@@ -0,0 +1,56 @@
+/**
+* C2C
+* - -
+* - Momento do disparo: Exibição do modal de 0800
+*/
+dataLayer.push({
+  event: 'feature-step',
+  feature: {
+    name: 'c2c',
+    type: 'c2c',
+    step: {
+      name: 'home',
+      index: 1,
+      type: 'initial',
+      success: true
+    }
+  }
+})
+
+/**
+* C2RC
+* - -
+* - Momento do disparo: Exibição do modal de sucesso de C2RC
+*/
+dataLayer.push({
+  event: 'feature-step',
+  feature: {
+    name: 'c2rc',
+    type: 'c2rc',
+    step: {
+      name: 'home',
+      index: 1,
+      type: 'initial',
+      success: true
+    }
+  }
+})
+
+/**
+* Checkout Success
+* - Wooza
+* - Momento do disparo: Exibição da tela de sucesso do checkout de Wooza
+*/
+dataLayer.push({
+  event: 'feature-step',
+  feature: {
+    name: 'checkout',
+    type: 'checkout',
+    step: {
+      name: 'success',
+      index: 4,
+      type: 'completion',
+      success: true
+    }
+  }
+})
